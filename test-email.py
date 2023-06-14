318699a7fbdf0e5246739f913d93998b356021aa
kvngstar <kvngsley019@gmail.com>
2023-06-14 22:35:13 +0100

    emailing with python
    My first python application

---
+++ test-email.py
@@ -0,0 +1,25 @@
+from email.message import EmailMessage
+
+import ssl
+import smtplib
+
+
+
+
+
+
+email_sender = 'kvngsley019@gmail.com'
+email_password = 'uxshthahpurbiynz'
+email_reciever = 'sekixaf374@bodeem.com'
+subject ="codewithkingsley is learning new things"
+body = """ 'Best python developer'"""
+em = EmailMessage()
+em['from'] = email_sender
+em['to'] = email_reciever
+em['subject'] = subject
+em.set_content(body)
+
+context =ssl.create_default_context()
+with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smpt:
+    smpt.login(email_sender,email_password)
+    smpt.sendmail(email_sender,email_reciever,em.as_string()  )
