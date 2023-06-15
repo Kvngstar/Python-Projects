2bece80520ca084fb84af8ff7b703a0ca756bcac
kvngstar <kvngsley019@gmail.com>
2023-06-15 01:11:53 +0100

    word-replacer program

---
+++ text-replacement-program.py
@@ -0,0 +1,8 @@
+
+random_text = "i am kingsley, I love python programming language"
+
+tobe_replaced = input("Enter a text to replace: ").lower()
+replacer = input("replace with: ").lower()
+
+result = random_text.replace(tobe_replaced,replacer)
+print(result)
